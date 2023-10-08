9bbc33a100e007c16370c5713eeb6c596a3137ae
Manuel Andrés Cota Santeliz <cota.manuel@rocketmail.com>
2023-10-08 08:28:56 -0700

    Added ids to each moonquake

--- src/app/data/dm/route.ts
+++ src/app/data/dm/route.ts
@@ -3,6 +3,7 @@ import { NextResponse } from "next/server"
 const dm = [
     {
         type: "Deep moonquake",
+        id: "b21b61ee-4a28-4321-b5a4-9d9840c0fdea",
         date: 118235400.0,
         lat: -17.4,
         long: -38.4,
@@ -10,6 +11,7 @@ const dm = [
     },
     {
         type: "Deep moonquake",
+        id: "9a489769-3b58-44c1-9ff4-be032978430f",
         date: 205177920.0,
         lat: 49.7,
         long: 54.7,
@@ -17,6 +19,7 @@ const dm = [
     },
     {
         type: "Deep moonquake",
+        id: "a8613cbc-3417-4ba5-9e55-1c89463b4545",
         date: 205150260.0,
         lat: 24,
         long: 53.7,
@@ -24,6 +27,7 @@ const dm = [
     },
     {
         type: "Deep moonquake",
+        id: "b721fd74-ee62-4558-af0c-b4f4b79b156a",
         date: 232653120.0,
         lat: -28,
         long: -28.1,
@@ -31,6 +35,7 @@ const dm = [
     },
     {
         type: "Deep moonquake",
+        id: "bcdd6040-cb03-45e6-a750-49b91eb55cd6",
         date: 230093880.0,
         lat: -37.8,
         long: -30.8,
@@ -38,6 +43,7 @@ const dm = [
     },
     {
         type: "Deep moonquake",
+        id: "980d1cc0-607d-4fc3-8ecd-d27817781500",
         date: 235483260.0,
         lat: 9.3,
         long: 17.5,
@@ -45,6 +51,7 @@ const dm = [
     },
     {
         type: "Deep moonquake",
+        id: "9a0396d2-fba7-4eb9-b714-cea0278d8bf7",
         date: 107488380.0,
         lat: -28.7,
         long: -33.9,
@@ -52,6 +59,7 @@ const dm = [
     },
     {
         type: "Deep moonquake",
+        id: "0e858b9e-4182-4c23-89db-7f6090c71479",
         date: 89999520.0,
         lat: 23.1,
         long: -18,
@@ -59,6 +67,7 @@ const dm = [
     },
     {
         type: "Deep moonquake",
+        id: "38e8b052-a587-48f6-a30e-0101087cdbfe",
         date: 95147400.0,
         lat: 18.6,
         long: 34.7,
@@ -66,6 +75,7 @@ const dm = [
     },
     {
         type: "Deep moonquake",
+        id: "b32cc4a2-208b-4530-a2fe-7231e5e13f1e",
         date: 74823480.0,
         lat: 21.7,
         long: -41,
@@ -73,6 +83,7 @@ const dm = [
     },
     {
         type: "Deep moonquake",
+        id: "28a76d62-c6f5-4d1e-a022-70b6d81a6752",
         date: 235012620.0,
         lat: -36.8,
         long: -38.9,
@@ -80,6 +91,7 @@ const dm = [
     },
     {
         type: "Deep moonquake",
+        id: "41fec499-aca9-465e-9294-ed1662799c82",
         date: 234760500.0,
         lat: 34.4,
         long: 59.3,
@@ -87,6 +99,7 @@ const dm = [
     },
     {
         type: "Deep moonquake",
+        id: "0576713a-7308-44ca-aa62-567c2b67322b",
         date: 232614060.0,
         lat: 22.5,
         long: 18.5,
@@ -94,6 +107,7 @@ const dm = [
     },
     {
         type: "Deep moonquake",
+        id: "a19c54cd-7f7b-44eb-9522-dd3b9666b286",
         date: 74936520.0,
         lat: 11.8,
         long: -34.3,
@@ -101,6 +115,7 @@ const dm = [
     },
     {
         type: "Deep moonquake",
+        id: "f116fc75-1471-4709-99d7-8b7f58fe7237",
         date: 87705300.0,
         lat: 6.9,
         long: 117.8,
@@ -108,6 +123,7 @@ const dm = [
     },
     {
         type: "Deep moonquake",
+        id: "cb077abd-a0ab-4245-8d02-861a2f8a71c5",
         date: 77420040.0,
         lat: 7,
         long: -9.3,
@@ -115,6 +131,7 @@ const dm = [
     },
     {
         type: "Deep moonquake",
+        id: "4e7e148e-c0b6-4f7f-a908-d5c4f42513b3",
         date: 110098080.0,
         lat: -1.6,
         long: -10.9,
@@ -122,6 +139,7 @@ const dm = [
     },
     {
         type: "Deep moonquake",
+        id: "50094abc-9a3c-4ee2-890b-fecae1b5a18d",
         date: 76893360.0,
         lat: 13.9,
         long: -26.8,
@@ -129,6 +147,7 @@ const dm = [
     },
     {
         type: "Deep moonquake",
+        id: "7a1ac3b8-6cd0-44b9-8ec3-c350554e7226",
         date: 105267120.0,
         lat: 22.7,
         long: -53.5,
@@ -136,6 +155,7 @@ const dm = [
     },
     {
         type: "Deep moonquake",
+        id: "7566502e-553a-4ddc-af40-c24092ee0345",
         date: 138190140.0,
         lat: 51.9,
         long: 57.1,
@@ -143,6 +163,7 @@ const dm = [
     },
     {
         type: "Deep moonquake",
+        id: "6c81b40e-3d2c-42eb-8011-3fa797b99e1a",
         date: 105005100.0,
         lat: 9.4,
         long: -51.6,
@@ -150,6 +171,7 @@ const dm = [
     },
     {
         type: "Deep moonquake",
+        id: "13c28983-dcbb-47ab-939b-1cc2d448961d",
         date: 130433700.0,
         lat: 8.8,
         long: 15.7,
@@ -157,6 +179,7 @@ const dm = [
     },
     {
         type: "Deep moonquake",
+        id: "ebbc38ee-e60a-467b-a2e3-572a3939669d",
         date: 232895280.0,
         lat: -3.4,
         long: 18.7,
--- src/app/data/normal/route.ts
+++ src/app/data/normal/route.ts
@@ -3,6 +3,7 @@ import { NextResponse } from "next/server"
 const sm = [
     {
         type: "Shallow moonquake",
+        id: "6631340e-f3b1-4f58-b293-9152b0eaafad",
         date: 40744855.0,
         lat: 48,
         long: 35,
@@ -10,6 +11,7 @@ const sm = [
     },
     {
         type: "Shallow moonquake",
+        id: "30ab5184-9cac-473d-8c93-6d9944723495",
         date: 43633510.0,
         lat: 42,
         long: -24,
@@ -17,6 +19,7 @@ const sm = [
     },
     {
         type: "Shallow moonquake",
+        id: "7113ffbe-9745-4e2d-ac42-9f7d3d852866",
         date: 48111885.0,
         lat: 43,
         long: -47,
@@ -24,6 +27,7 @@ const sm = [
     },
     {
         type: "Shallow moonquake",
+        id: "b709a8d6-1c0e-44ad-8c2d-c0d3933fa4f9",
         date: 63264580.0,
         lat: 54,
         long: 101,
@@ -31,6 +35,7 @@ const sm = [
     },
     {
         type: "Shallow moonquake",
+        id: "f427f089-b79b-4862-9d35-93136cad7834",
         date: 85613755.0,
         lat: 12,
         long: 46,
@@ -38,6 +43,7 @@ const sm = [
     },
     {
         type: "Shallow moonquake",
+        id: "bfa9e64e-9778-4e89-829f-0f98cf5dbc66",
         date: 92556500.0,
         lat: 51,
         long: 45,
@@ -45,6 +51,7 @@ const sm = [
     },
     {
         type: "Shallow moonquake",
+        id: "a3945e0c-f308-4f0c-be62-38372046c2cf",
         date: 92746215.0,
         lat: -20,
         long: -80,
@@ -52,6 +59,7 @@ const sm = [
     },
     {
         type: "Shallow moonquake",
+        id: "38333295-50c9-4719-84ee-fa41c73b7f8c",
         date: 98085130.0,
         lat: 33,
         long: 35,
@@ -59,6 +67,7 @@ const sm = [
     },
     {
         type: "Shallow moonquake",
+        id: "30b9754b-67ca-4699-85cb-f203221fa541",
         date: 100882590.0,
         lat: -84,
         long: -134,
@@ -66,6 +75,7 @@ const sm = [
     },
     {
         type: "Shallow moonquake",
+        id: "e40a016b-1ad1-460a-9b48-b17d2fdc99a1",
         date: 109480920.0,
         lat: -1,
         long: -71,
@@ -73,6 +83,7 @@ const sm = [
     },
     {
         type: "Shallow moonquake",
+        id: "d22b4b92-e125-4240-a0a5-72e55ba913df",
         date: 118321080.0,
         lat: -37,
         long: -29,
@@ -80,6 +91,7 @@ const sm = [
     },
     {
         type: "Shallow moonquake",
+        id: "a99e5d01-70af-47c8-8d7e-2dff6428a523",
         date: 130911410.0,
         lat: 36,
         long: -16,
@@ -87,6 +99,7 @@ const sm = [
     },
     {
         type: "Shallow moonquake",
+        id: "fb317127-cb19-4398-b0c6-f79758a1cbb6",
         date: 133632660.0,
         lat: -48,
         long: -106,
@@ -94,6 +107,7 @@ const sm = [
     },
     {
         type: "Shallow moonquake",
+        id: "3842b482-43f0-4778-8e30-7bdf3f58aef4",
         date: 135635715.0,
         lat: -37,
         long: 42,
@@ -101,13 +115,7 @@ const sm = [
     },
     {
         type: "Shallow moonquake",
-        date: 139117335.0,
-        lat: ,
-        long: ,
-        mag: 0.6
-    },
-    {
-        type: "Shallow moonquake",
+        id: "41637af8-860f-4d48-839e-a555a2705de8",
         date: 142760790.0,
         lat: 21,
         long: 88,
@@ -115,6 +123,7 @@ const sm = [
     },
     {
         type: "Shallow moonquake",
+        id: "223393be-a679-4de8-980e-aae8d2c4c7b4",
         date: 157970520.0,
         lat: 29,
         long: -98,
@@ -122,6 +131,7 @@ const sm = [
     },
     {
         type: "Shallow moonquake",
+        id: "0cc8f832-0a7e-49c2-b78a-7de312e3d49b",
         date: 158753650.0,
         lat: 75,
         long: 40,
@@ -129,6 +139,7 @@ const sm = [
     },
     {
         type: "Shallow moonquake",
+        id: "ad342c14-d1e0-4a96-ad4b-ea2a2ccce83d",
         date: 158829980.0,
         lat: -2,
         long: -51,
@@ -136,6 +147,7 @@ const sm = [
     },
     {
         type: "Shallow moonquake",
+        id: "384d9735-e668-4bde-b986-8817d195a4f3",
         date: 161586230.0,
         lat: -19,
         long: -26,
@@ -143,6 +155,7 @@ const sm = [
     },
     {
         type: "Shallow moonquake",
+        id: "fda077b2-8115-41a9-8af2-5d5b70dd9b55",
         date: 168701825.0,
         lat: -49,
         long: -45,
@@ -150,6 +163,7 @@ const sm = [
     },
     {
         type: "Shallow moonquake",
+        id: "106139cd-e53c-4496-bf91-aa8153eb70d3",
         date: 170490540.0,
         lat: 3,
         long: -58,
@@ -157,6 +171,7 @@ const sm = [
     },
     {
         type: "Shallow moonquake",
+        id: "c22e563d-3c52-48f2-87fe-b4a47e1ce423",
         date: 184863175.0,
         lat: -8,
         long: 64,
@@ -164,6 +179,7 @@ const sm = [
     },
     {
         type: "Shallow moonquake",
+        id: "ae5f7b08-9248-4970-a9ce-40e4fecd7a6b",
         date: 189627535.0,
         lat: 50,
         long: 30,
@@ -171,6 +187,7 @@ const sm = [
     },
     {
         type: "Shallow moonquake",
+        id: "9c55b0f2-8d8d-4102-babb-373238209916",
         date: 190307885.0,
         lat: 38,
         long: 44,
@@ -178,6 +195,7 @@ const sm = [
     },
     {
         type: "Shallow moonquake",
+        id: "b5529653-b922-4fde-a3fc-8d2a0f7c33ed",
         date: 194980360.0,
         lat: 50,
         long: -20,
@@ -185,6 +203,7 @@ const sm = [
     },
     {
         type: "Shallow moonquake",
+        id: "b94a1636-b8de-4577-a94e-8bc28f832521",
         date: 195169330.0,
         lat: -19,
         long: -12,
@@ -192,6 +211,7 @@ const sm = [
     },
     {
         type: "Shallow moonquake",
+        id: "4dfba247-fcb8-4e11-9349-0cc98c8d9743",
         date: 201123160.0,
         lat: 77,
         long: -10,
@@ -201,6 +221,7 @@ const sm = [
 const ai = [
     {
         type: "Artificial Impact (12 LM)",
+        id: "a69f86e8-6a79-4aec-b581-03e285ce04cd",
         year: 69,
         day: 324,
         h: 22,
@@ -212,6 +233,7 @@ const ai = [
     },
     {
         type: "Artificial Impact (13 S-IVB)",
+        id: "e231c310-c329-47cf-b416-bafb977202fd",
         year: 70,
         day: 105,
         h: 1,
@@ -223,6 +245,7 @@ const ai = [
     },
     {
         type: "Artificial Impact (14 S-IVB)",
+        id: "e0768b89-7057-4218-8514-ec91fd92d217",
         year: 71,
         day: 35,
         h: 7,
@@ -234,6 +257,7 @@ const ai = [
     },
     {
         type: "Artificial Impact (14 LM)",
+        id: "c1d4a13b-f9a4-4693-8ec2-4b6ccbb1591b",
         year: 71,
         day: 38,
         h: 0,
@@ -245,6 +269,7 @@ const ai = [
     },
     {
         type: "Artificial Impact (15 S-IVB)",
+        id: "b49d7796-a7c1-45b9-8092-10ff1bb0a326",
         year: 71,
         day: 210,
         h: 20,
@@ -256,6 +281,7 @@ const ai = [
     },
     {
         type: "Artificial Impact (15 LM)",
+        id: "5bfc6404-8819-4b5a-98ac-4711c38e0242",
         year: 71,
         day: 215,
         h: 3,
@@ -267,6 +293,7 @@ const ai = [
     },
     {
         type: "Artificial Impact (17 S-IVB)",
+        id: "20a51d6b-266d-4a9f-bbe4-39bd2f330813",
         year: 72,
         day: 345,
         h: 20,
@@ -278,6 +305,7 @@ const ai = [
     },
     {
         type: "Artificial Impact (17 LM)",
+        id: "62c3afd2-7d7d-487f-95d3-532aca73d89b",
         year: 72,
         day: 350,
         h: 6,
