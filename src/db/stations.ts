5e41538cb9506fae15deb7f6b73009a0fb4c3f2d
Manuel Andrés Cota Santeliz <cota.manuel@rocketmail.com>
2023-10-12 00:14:08 -0700

    Updated date format in all endpoints

--- src/db/stations.ts
+++ src/db/stations.ts
@@ -4,40 +4,60 @@ const stations = [
         mission: "Apollo 11",
         lat: 0.67337,
         long: 23.473146,
-        startTime: '1969-07-21',
-        endTime: '1969-08-26'
+        startYear: 1969,
+        startMonth: 7,
+        startDay: 21,
+        endYear: 1969,
+        endMonth: 8,
+        endDay: 26
     },
     {
         id: "a12",
         mission: "Apollo 12",
         lat: -3.01084,
         long: -23.42456,
-        startTime: '1969-11-19',
-        endTime: '1977-09-30'
+        startYear: 1969,
+        startMonth: 11,
+        startDay: 19,
+        endYear: 1977,
+        endMonth: 9,
+        endDay: 30
     },
     {
         id: "a14",
         mission: "Apollo 14",
         lat: -3.6445,
         long: -17.47753,
-        startTime: '1971-02-05',
-        endTime: '1977-09-30'
+        startYear: 1971,
+        startMonth: 2,
+        startDay: 5,
+        endYear: 1977,
+        endMonth: 9,
+        endDay: 30
     },
     {
         id: "a15",
         mission: "Apollo 15",
         lat: 26.13407,
         long: 3.62981,
-        startTime: '1971-07-31',
-        endTime: '1977-09-30'
+        startYear: 1971,
+        startMonth: 7,
+        startDay: 31,
+        endYear: 1977,
+        endMonth: 9,
+        endDay: 30
     },
     {
         id: "a16",
         mission: "Apollo 16",
         lat: -8.97577,
         long: 23.473146,
-        startTime: '1972-04-21',
-        endTime: '1977-09-30'
+        startYear: 1972,
+        startMonth: 4,
+        startDay: 21,
+        endYear: 1977,
+        endMonth: 9,
+        endDay: 30
     },
 ]
 
